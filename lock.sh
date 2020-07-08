3aa5e44dd83802e52fafb15ace0fea1282862a5b
YukinaMochizuki <1p41p4jejo@gmail.com>
2020-07-09 05:01:29 +0800

    Init Repo

---
+++ lock.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Overlays neofetch stats on a blurred lock screen
+
+# Turns composite off
+# killall compton
+
+# makes the neofetch, and scrots it (delay there for proccesing, change it to your pc's speed)
+# urxvt +sb -name lock -e neofetch --song_shorthand on --source ~/Images/fetch/1197b22-1.jpg --scrot_cmd 'scrot -z -u -m -d 0 /tmp/lock.png' -s & sleep 1.7; kill $!
+
+urxvt +tr -e sh -c "neofetch;i3-msg floating enable > /dev/zero;scrot -o -u -d 1 /tmp/lock.png;sleep 2"&sleep 1&
+
+# turns compton back on, and waits a sec (again, just giving it a sec to load)
+# compton -b & sleep .3
+
+# crops image, this is hardcoded since I didn't want to deal with %'s, so change based on your screen size
+# convert /tmp/lock.png -crop 490x245+0+0 /tmp/lock.png
+
+# Define image locations
+FOLDER=`dirname "$BASH_SOURCE"`
+ICON=/tmp/lock.png
+TMPBG=/tmp/screen.png
+
+# takes a screenshot
+scrot -o /tmp/screen.png
+
+# blurs it and overlays 
+convert $TMPBG -blur 0x10 $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+
+# actually locks it
+# i3lock -u -e -i $TMPBG
+# bash $FOLDER/lock2 -i $TMPBG
+i3lock -u -i $TMPBG
